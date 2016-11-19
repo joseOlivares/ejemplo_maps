98e86addd19220d7705e468cc6e0476286e527d6
José Luis Olivares <joseluis_503@yahoo.com>
2016-11-19 09:43:45 -0600

    Add files via upload
    Usando Socket IO

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public')); //serving statics files like css, js, images
 
-var port=process.env.PORT || 3000; //this is for heroku enviroment
+var port=process.env.PORT || 3000; //this is for heroku
 
 
 app.get('/', function(req, res){
@@ -18,4 +18,16 @@ http.listen(port, function(){
 });
 
 
+//trabajando con Socket IO
+var socketCount = 0; //contador de conexiones
 
+
+io.on('connection', function(socket){
+
+	console.log('Usuario Conectado...');
+	//console.log(socket);
+    socketCount++;// Socket has connected, increase socket count
+	io.sockets.emit('usuario conectado', socketCount);    // Let all sockets know how many are connected
+
+
+}); //cierra on connection
